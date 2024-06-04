47517f41acce797240268a96c0f5a1c252d67772
SteffenHelmke <steffen.helmke@informatik.hs-fulda.de>
2024-06-04 14:54:59 +0200

    Holespawn Fixed, Bombs and Faster Balls added

---
+++ Assets/FallingBalls/Scripts/DestroyBomb.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyFalseYellow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(transform.position.y >= 3.5) return;
+        if(transform.position.y > 0.5) {
+            GameObject.Find("ScriptObject").GetComponent<Gamelogic>().score -= 15;
+        }
+        Destroy(this.gameObject);
+    }
+}
