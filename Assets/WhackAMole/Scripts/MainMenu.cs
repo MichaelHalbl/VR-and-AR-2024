85d2d578fa8298f3e0d3557b483a920746e02a8c
carelledjuffo <76751158+carelledjuffo@users.noreply.github.com>
2024-09-11 17:18:08 +0200

    Add background image

--- Assets/WhackAMole/Scripts/MainMenu.cs
+++ Assets/WhackAMole/Scripts/MainMenu.cs
@@ -4,30 +4,10 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Method to start the first game
-    public void StartGame1()
+    public void StartGame()
     {
-        Debug.Log("Start Game 1 clicked");
+        Debug.Log("Start Game clicked");
        // SceneManager.LoadScene("GameScene1"); 
-    }
-
-    // Method to start the second game
-    public void StartGame2()
-    {
-        Debug.Log("Start Game 2 clicked");
-        //SceneManager.LoadScene("GameScene2"); 
-    }
-    // Method to start the third game
-    public void StartGame3()
-    {
-        Debug.Log("Start Game 3 clicked");
-       // SceneManager.LoadScene("GameScene3"); 
-    }
-
-    // Method to start the fourth game
-    public void StartGame4()
-    {
-        Debug.Log("Start Game 4 clicked");
-       // SceneManager.LoadScene("GameScene4"); 
     }
 
     // Method to quit the game
